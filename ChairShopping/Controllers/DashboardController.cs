e3c75fa9a4bf895f4321f3b525986466a7af0022
MahmoudOsama31 <mahmoudosama3112000@gmail.com>
2023-09-28 12:57:45 +0300

    Create Users in dashboard

--- ChairShopping/Controllers/DashboardController.cs
+++ ChairShopping/Controllers/DashboardController.cs
@@ -1,12 +1,88 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ChairShopping.Data;
+using ChairShopping.Interfaces;
+using ChairShopping.ViewModel;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ChairShopping.Controllers
 {
 	public class DashboardController : Controller
 	{
-		public IActionResult Index()
+        private readonly IAdmin _repo;
+
+        public DashboardController(IAdmin repo)
+        {
+			_repo = repo;
+        }
+        public IActionResult Index()
 		{
 			return View();
 		}
-	}
+		public async Task<IActionResult> GetAllUsers()
+		{
+			ViewBag.AllUsers =await _repo.GetAllUsers();
+			return View();
+		}
+        public IActionResult AddUser()
+        {
+            return View();
+        }
+		[HttpPost]
+		public async Task<ActionResult<ApplicationUser>> AddUser(RegisterVM model)
+		{
+			if (ModelState.IsValid)
+			{
+				await _repo.AddUser(model);
+				return RedirectToAction("GetAllUsers");
+			}
+			return View();
+		}
+        public async Task<ActionResult<ApplicationUser>> EditUser(string id)
+        {
+            if (id != null)
+            {
+                var user = await _repo.GetUserById(id);
+                if (user != null)
+                {
+                    ViewBag.UpdateUser = user;
+                }
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult<ApplicationUser>> EditUser(RegisterVM model, string id)
+        {
+            await _repo.EditUser(model, id);
+            return RedirectToAction("GetAllUsers", "Dashboard");
+        }
+        public async Task<ActionResult> DeleteUser(string id)
+        {
+            if (id != null)
+            {
+                var user = await _repo.GetUserById(id);
+                if (user != null)
+                {
+                    ViewBag.UpdateUser = user;
+                }
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult<ApplicationUser>> DeleteUser(string id, RegisterVM model)
+        {
+            await _repo.DeleteUser(id);
+            return RedirectToAction("GetAllUsers", "Dashboard");
+        }
+        public async Task<ActionResult<ApplicationUser>> GetUserDetails(string id)
+        {
+            if (id != null)
+            {
+                var user = await _repo.GetUserById(id);
+                if (user != null)
+                {
+                    ViewBag.UpdateUser = user;
+                }
+            }
+            return View();
+        }
+    }
 }
---
+++ ChairShopping/Repositories/Admin.cs
@@ -0,0 +1,92 @@
+﻿using ChairShopping.Data;
+using ChairShopping.Interfaces;
+using ChairShopping.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace ChairShopping.Repositories
+{
+    public class Admin : IAdmin
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+        private readonly AppDbContext _db;
+
+
+        public Admin(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, AppDbContext db, RoleManager<ApplicationRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+            _db = db;
+        }
+        public async Task<IEnumerable<ApplicationUser>> GetAllUsers()
+        {
+            var users = await _db.Users.ToListAsync();
+            if (users == null)
+            {
+                return null;
+            }
+            return users;
+        }
+        public async Task<ApplicationUser> AddUser(RegisterVM model)
+        {
+            if (model == null)
+            {
+                return null;
+            }
+            var exist = await _userManager.FindByEmailAsync(model.Email);
+            if (exist != null)
+            {
+                return null;
+            }
+            var user = new ApplicationUser
+            {
+                UserName = model.UserName,
+                Email = model.Email,
+                IsAgree = model.IsAgree               
+            };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                return user;
+            }
+            return null;
+        }
+
+        public async Task<ApplicationUser> EditUser(RegisterVM model, string id)
+        {
+            var user = await _db.Users.FirstOrDefaultAsync(x => x.Id == id);
+            if (user == null)
+            {
+                return null;
+            }
+            user.UserName = model.UserName;
+            user.Email = model.Email;
+            user.PasswordHash = model.Password;
+            user.IsAgree = model.IsAgree;
+            _db.Users.Update(user);
+            await _db.SaveChangesAsync();
+            return user;
+        }
+
+        public Task<ApplicationUser> GetUserById(string id)
+        {
+            return _db.Users.FirstOrDefaultAsync(u => u.Id == id);
+        }
+
+        public async Task<ApplicationUser> DeleteUser(string id)
+        {
+            var user = await _db.Users.FirstOrDefaultAsync(x => x.Id == id);
+            if (user == null)
+            {
+                return null;
+            }
+            _db.Users.Remove(user);
+            await _db.SaveChangesAsync();
+            return user;
+        }
+    }
+}
