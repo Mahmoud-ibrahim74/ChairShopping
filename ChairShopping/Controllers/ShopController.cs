3204256d6cffbe8ce955aaa9f5d9698321f72dc0
Mahmoud Ibrahm <mhmoud.ibrahim74@gmail.com>
2023-10-24 20:39:21 +0300

    shop page

---
+++ ChairShopping/Controllers/ShopController.cs
@@ -0,0 +1,20 @@
+﻿using ChairShopping.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChairShopping.Controllers
+{
+	public class ShopController : Controller
+	{
+		private readonly IAdmin _admin;
+		public ShopController(IAdmin admin)
+		{
+			this._admin = admin;
+		}
+
+		public IActionResult Index()
+		{
+			var result = _admin.GetAllProducts().Result.Take(8).ToList();
+			return View(result);
+		}
+	}
+}
